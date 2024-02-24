30b554cd115c1e1a016b8212224511ac1241f6cf
Tom <t.meulensteen@gmail.com>
2024-02-24 21:40:27 +0100

    Update runPortainer.sh
    aanpassingen, want netter

--- runPortainer.sh
+++ runPortainer.sh
@@ -2,7 +2,15 @@
 docker volume create portainer_data
 docker stop portainer
 docker rm portainer
-docker run -d -p 8000:8000 -p 9000:9000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer:1.24.2-alpine --no-auth
+docker run -d \
+  --name portainer \
+  --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v portainer_data:/data \
+  -p 8000:8000 \
+  -p 9000:9000 \
+  -p 9443:9443 \
+  portainer/portainer:1.24.2-alpine --no-auth
 # Do not use portainer-ce https://github.com/portainer/portainer/issues/2101#issuecomment-666070463
 
 
